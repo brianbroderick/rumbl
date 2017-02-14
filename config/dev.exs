81c970dc3e58a28928c40bf69e2e306384a3b93c
Brian Broderick <brianbroderick19@gmail.com>
2017-02-13 19:51:16 -0700

    mix ecto.create

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :lensformation, Lensformation.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "lensformation_dev",
+  username: "brian.broderick",
+  # password: "postgres",
+  database: "lf_dev",
   hostname: "localhost",
   pool_size: 10
