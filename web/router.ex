da62f5156d298ab61e95871dbc5312b7e483a19a
Brian Broderick <brianbroderick19@gmail.com>
2017-02-21 21:09:04 -0700

    Add Login

--- web/router.ex
+++ web/router.ex
@@ -7,6 +7,7 @@ defmodule Lensformation.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
+    plug Lensformation.Auth, repo: Lensformation.Repo
   end
 
   pipeline :api do
@@ -16,10 +17,8 @@ defmodule Lensformation.Router do
   scope "/", Lensformation do
     pipe_through :browser # Use the default browser stack
 
-    get "/users", UserController, :index
-    get "/users/:id", UserController, :show
-
     get "/", PageController, :index
+    resources "/users", UserController, only: [:index, :show, :new, :create]
   end
 
   # Other scopes may use custom stacks.
