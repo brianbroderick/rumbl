491c83114b22cd4d1d95cc4393adc537c4753a08
Brian Broderick <brianbroderick19@gmail.com>
2017-03-13 21:58:41 -0600

    Add video controller

--- web/models/user.ex
+++ web/models/user.ex
@@ -6,6 +6,7 @@ defmodule Lensformation.User do
     field :username, :string
     field :password, :string, virtual: true
     field :password_hash, :string
+    has_many :videos, Lensformation.Video
 
     timestamps()
   end
